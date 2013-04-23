494ab804893720dcfe7ed156ccb5bcb88608571c
xingzhong <xu.xeno@gmail.com>
2013-04-23 15:03:50 -0400

    update the leader borad through dict

--- leader.py
+++ leader.py
@@ -7,9 +7,35 @@ class Player(object):
   def getTotalPoints(self, user):
     return sum(self._dict.get(user, []))
 
+from datetime import datetime
+class leader (dict):
+  def addPoints(self, user, point):
+    if self.has_key(user):
+      self[user]["pointer"] = self[user]["pointer"] + 1
+      if self[user]["pointer"] > 7 :
+        self[user]["pointer"] = 0
+      self[user]["points"][self[user]["pointer"]] = point
+    else:
+      self[user] = {}
+      self[user]["points"] = [point, 0, 0, 0, 0, 0, 0]
+      self[user]["pointer"] = 0
+    self[user]['timestamp'] = datetime.now()
+
+  def getTotalPoints(self, user):
+    return sum( self[user]["points"] )
+
 if __name__ == "__main__":
   p = Player()
   p.addPoints("John", 10)
   p.addPoints("Xu", 5)
   print p._dict
   print p.getTotalPoints("John")
+
+  l = leader()
+  l.addPoints("Xu", 5)
+  l.addPoints("Xu", 6)
+  l.addPoints("John", 4)
+
+  print l
+
+  print l.getTotalPoints("Xu")
